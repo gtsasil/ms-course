671adc70fc1563e186b67cf9e17ed5e8ad22c97e
Gleisson Santos Silva <gtsasil@gmail.com>
2021-10-16 22:58:35 +0100

    Feign Client

--- hr-payroll/src/main/java/com/gtsasil/payroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/gtsasil/payroll/services/PaymentService.java
@@ -1,30 +1,21 @@
 package com.gtsasil.payroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.gtsasil.payroll.entities.Payment;
 import com.gtsasil.payroll.entities.Worker;
+import com.gtsasil.payroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
-	
-	@Value("${hr-worker.host}")
-	private String workerHost;
-	
+		
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
 	
 	public Payment getPayment(long workerId, int days) {
-		Map<String, String> uriVariable = new HashMap<>();
-		uriVariable.put("id", "" + workerId);
 		
-		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariable);
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		return new Payment(worker.getName(), worker.getDaily_Income(), days);
 	}
 
