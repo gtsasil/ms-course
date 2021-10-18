1346764f33ae682fe7ab41b121d597edc9d2be12
Gleisson Santos Silva <gtsasil@gmail.com>
2021-10-18 10:33:32 +0100

    Updated POM of hr-api-gatway-zull

---
+++ hr-api-gateway-zuul/src/main/java/com/gtsasil/hrapigatewayzuul/HrApiGatewayZuulApplication.java
@@ -0,0 +1,18 @@
+package com.gtsasil.hrapigatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class HrApiGatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrApiGatewayZuulApplication.class, args);
+	}
+
+}
--- hr-payroll/src/main/java/com/gtsasil/payroll/entities/Worker.java
+++ hr-payroll/src/main/java/com/gtsasil/payroll/entities/Worker.java
@@ -43,17 +43,42 @@ public class Worker {
 		this.dailyIncome = dailyIncome;
 	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Worker)) return false;
-        Worker worker = (Worker) o;
-        return getId().equals(worker.getId()) && Objects.equals(getName(), worker.getName()) && Objects.equals(getDailyIncome(), worker.getDailyIncome());
-    }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dailyIncome == null) ? 0 : dailyIncome.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId(), getName(), getDailyIncome());
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Worker other = (Worker) obj;
+		if (dailyIncome == null) {
+			if (other.dailyIncome != null)
+				return false;
+		} else if (!dailyIncome.equals(other.dailyIncome))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
 }
 
